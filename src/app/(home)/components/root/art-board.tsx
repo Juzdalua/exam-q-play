28bf1b2c3c23125c9b9e1690bf03ffff9eed0dfe
Juzdalua <bearwcw@naver.com>
2024-12-12 13:50:27 +0900

    Update main photo size

--- src/app/(home)/components/root/art-board.tsx
+++ src/app/(home)/components/root/art-board.tsx
@@ -1,9 +1,9 @@
 const ArtBoard = () => {
   return (
     <div className="flex justify-center">
-      <div className="mt-20 flex flex-col items-center h-[650px] w-[400px]">
+      <div className="mt-20 flex flex-col items-center h-[620px] w-[400px]">
         <img className="h-[550px] object-contain rounded-box" src={process.env.NEXT_PUBLIC_MAIN_PHOTO_URL} alt="asd" />
-        <div className="flex flex-col items-end justify-center w-full pr-10 h-full">
+        <div className="flex flex-col items-end justify-end w-full pr-10 h-full">
           <span className="text-xl">12321321312</span>
           <span className="text-xl">12321321312</span>
         </div>
