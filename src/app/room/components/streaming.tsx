269cea95756ab4dec3808a1e475c772e598095df
Juzdalua <bearwcw@naver.com>
2024-12-13 15:37:32 +0900

    Update custom Server & WS

--- src/app/room/components/streaming.tsx
+++ src/app/room/components/streaming.tsx
@@ -1,6 +1,32 @@
 "use client";
 
+import { useEffect } from "react";
+
 const Streaming = () => {
+  useEffect(() => {
+    const socket = new WebSocket("ws://localhost:8080");
+
+    socket.onopen = () => {
+      console.log("Connected to server");
+      socket.send("Hello, server!"); // Send Message
+    };
+
+    socket.onmessage = (event) => {
+      console.log("Message from server:", event.data);
+    };
+
+    socket.onerror = (error) => {
+      console.error("WebSocket error:", error);
+    };
+
+    socket.onclose = () => {
+      console.log("Connection closed");
+    };
+
+    return () => {
+      socket.close();
+    };
+  }, []);
   return <div className="border-2 border-red-500 min-h-80"></div>;
 };
 
---
+++ src/server.ts
@@ -0,0 +1,31 @@
+import { createServer } from "http";
+import { parse } from "url";
+import next from "next";
+import { WebSocketServer } from "ws";
+
+const dev = process.env.NODE_ENV != "production";
+const app = next({ dev });
+const handle = app.getRequestHandler();
+const port = process.env.PORT ?? 3000;
+
+app.prepare().then(() => {
+  const server = createServer((req, res) => {
+    const parsedUrl = parse(req.url!, true);
+    handle(req, res, parsedUrl);
+  });
+
+  const wss = new WebSocketServer({ port:process.env.WS_PORT ?? 8080 });
+
+  wss.on("connection", (ws) => {
+    console.log("New WebSocket connection");
+
+    ws.on("message", (message) => {
+      console.log("Received:", message.toString());
+      ws.send("Hello from server!");
+    });
+  });
+
+  server.listen(port, () => {
+    console.log(`✅ Ready on ${process.env.NEXT_PUBLIC_BASE_URL}:${port}`);
+  });
+});
