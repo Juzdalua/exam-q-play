55ec5ae46a60c4ea2003949937b253effa54c6ed
juzdalua <bearwcw@naver.com>
2025-01-21 18:10:38 +0900

    Update Pay-letter page with button

--- src/app/pay-letter/components/index.tsx
+++ src/app/pay-letter/components/index.tsx
@@ -1,40 +1,38 @@
 "use client";
 
 import { PaymentCode } from "@/src/utils/pay-letter/payLetterTypes";
-import { useEffect, useState } from "react";
+
+const HandleClickBtn = async (e: React.FormEvent) => {
+  e.preventDefault();
+
+  const res = await fetch(`/api/pay-letter/req-payment`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      pgcode: PaymentCode.CREDIT_CARD,
+      amount: 1000,
+      product_name: "테스트상품",
+      user_id: "jun",
+    }),
+  });
+
+  if (res && res.status == 200) {
+    const result = await res.json();
+    console.log(result);
+    window.open(result.data.online_url);
+  }
+};
 
 const PayLetterIndex = () => {
-  const [paymentData, setPaymentData] = useState(null);
-  useEffect(() => {
-    const fetchPayment = async () => {
-      const res = await fetch(`/api/pay-letter/req-payment`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          pgcode: PaymentCode.CREDIT_CARD,
-          amount: 1000,
-          product_name: "테스트상품",
-          user_id: "jun"
-        }),
-      });
-
-      if (res && res.status == 200) {
-        const result = await res.json();
-        setPaymentData(result.data);
-      }
-    };
-
-    fetchPayment();
-  }, []);
-
-  useEffect(() => {
-    if (!paymentData) return;
-    window.open(paymentData.online_url);
-  }, [paymentData]);
-
-  return <div></div>;
+  return (
+    <div className="flex items-center justify-center">
+      <button className="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg mt-5" onClick={HandleClickBtn}>
+        Pay
+      </button>
+    </div>
+  );
 };
 
 export default PayLetterIndex;
